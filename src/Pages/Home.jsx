15e9128b3f92617a0556eca7c15e2ac07ae04972
ashokdhinda <ashokkudhinda143@gmail.com>
2024-10-01 09:47:17 +0530

    banner part completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import { Box, Image } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <Navbar/>
-  )
+    <>
+      <Navbar />
+      <Box overflow={"hidden"}>
+        <Box p={1}>
+          <Image src="https://static5.lenskart.com/media/uploads/festive-desk-2-29Sep.png" />
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default Home
--- src/components/Navbar/DropDownNav.jsx
+++ src/components/Navbar/DropDownNav.jsx
@@ -3,7 +3,6 @@ import {
   Avatar,
   Box,
   Button,
-  Center,
   Flex,
   Heading,
   Image,
