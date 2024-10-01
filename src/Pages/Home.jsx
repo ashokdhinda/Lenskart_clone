08cff6e5c0f582c73b37d04d5daab438e3d3b731
ashokdhinda <ashokkudhinda143@gmail.com>
2024-10-01 12:27:55 +0530

    create some banner  all   part  completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
-import { Box, Image,Text } from "@chakra-ui/react";
+import { Box, Flex, Image,SimpleGrid,Text } from "@chakra-ui/react";
 import SliderOne from '../components/SliderS/SliderOne';
 import SliderTwo from '../components/SliderS/SliderTwo';
 import { Link } from 'react-router-dom';
@@ -116,6 +116,175 @@ const Home = () => {
             />
           </Link>
         </Box>
+
+        <Box
+          textAlign={{ base: "center", md: "center" }}
+          m={2}
+          fontFamily={"sans-serif"}
+        >
+          <Text as="h3" size="lg" fontSize={[15, 15, 20, 25]}>
+            {windowWidth <= 768
+              ? "----TRENDING SUNGLASSES----"
+              : "_____________________________TRENDING SUNGLASSES___________________________"}
+          </Text>
+        </Box>
+        <Box p={1}>
+          <img
+            src="https://static1.lenskart.com/media/desktop/img/Jan23/sunglasses/Sun-Banner-web.gif "
+            alt="TRENDING SUNGLASSES"
+          />
+        </Box>
+
+        <Box
+          textAlign={{ base: "center", md: "center" }}
+          m={2}
+          fontFamily={"sans-serif"}
+        >
+          <Text as="h3" size="lg" fontSize={[15, 15, 20, 25]}>
+            {windowWidth <= 768
+              ? "----------BOOK EYE TEST AT HOME-----------"
+              : "________________________________ BOOK EYE TEST AT HOME________________________________"}
+          </Text>
+        </Box>
+        <Box p={1}>
+          <img
+            src="https://static5.lenskart.com/media/uploads/hechome11.png"
+            alt="BOOK EYE TEST AT HOME"
+          />
+        </Box>
+        <Box
+          textAlign={{ base: "center", md: "center" }}
+          m={2}
+          fontFamily={"sans-serif"}
+        >
+          <Text as="h3" size="lg" fontSize={[15, 15, 20, 25]}>
+            {windowWidth <= 768
+              ? "--AQUACOLOR - GLAM UP WITH COLOR LENSES--"
+              : "_______________________AQUACOLOR - GLAM UP WITH COLOR LENSES______________________"}
+          </Text>
+        </Box>
+        <Box p={1}>
+          <img
+            src=" https://static1.lenskart.com/media/desktop/img/Oct22/kiara/Refresh-Banner-Web.gif"
+            alt="AQUACOLOR - GLAM UP WITH COLOR LENSES"
+          />
+        </Box>
+
+        <Box
+          textAlign={{ base: "center", md: "center" }}
+          m={2}
+          fontFamily={"sans-serif"}
+        >
+          <Text as="h3" size="lg" fontSize={[15, 15, 20, 25]}>
+            {windowWidth <= 768
+              ? "--------FIND THE PERFECT FIT--------"
+              : "___________________FIND THE PERFECT FIT_________________"}
+          </Text>
+        </Box>
+        <Flex gap={5} w={"80%"} margin={"auto"}>
+          <SimpleGrid gap={2} w={"50%"}>
+            <img
+              src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/eye-square10.jpg  "
+              alt="left 1st"
+            />
+            <img
+              src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/power-sun-square.jpg"
+              alt="left2nd"
+            />
+          </SimpleGrid>
+          <SimpleGrid gap={2} w={"50%"}>
+            <img
+              src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/ce-square.jpg"
+              alt="right1st"
+            />
+            <img
+              src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/sun-square.jpg"
+              alt="right2nd"
+            />
+            <img
+              src="https://static1.lenskart.com/media/desktop/img/Nov20/25-Nov/Banner03_TileDesktop.jpg"
+              alt="right3rd"
+            />
+          </SimpleGrid>
+        </Flex>
+        <Box
+          textAlign={{ base: "center", md: "center" }}
+          m={2}
+          fontFamily={"sans-serif"}
+        >
+          <Text as="h3" size="lg" fontSize={[15, 15, 20, 25]}>
+            {windowWidth <= 768
+              ? "--- INTRODUCING OJOS WEAR - SUBSCRIBE & SAVE---"
+              : "______________________INTRODUCING OJOS WEAR - SUBSCRIBE & SAVE ________________________"}
+          </Text>
+        </Box>
+        <Box p={1}>
+          <img
+            src=" https://static1.lenskart.com/media/desktop/img/May22/ojos-web.jpg"
+            alt=" INTRODUCING OJOS WEAR - SUBSCRIBE & SAVE "
+          />
+        </Box>
+        <Box
+          textAlign={{ base: "center", md: "center" }}
+          m={2}
+          fontFamily={"sans-serif"}
+        >
+          <Text as="h3" size="lg" fontSize={[15, 15, 20, 25]}>
+            {windowWidth <= 768
+              ? "--- CONTACT LENSES & MORE---"
+              : "________________________CONTACT LENSES & MORE___________________"}
+          </Text>
+        </Box>
+
+        <SimpleGrid
+          gridTemplateColumns={"repeat(2,1fr)"}
+          w={"70%"}
+          m={"auto"}
+          gap={5}
+        >
+          <img
+            src=" https://static1.lenskart.com/media/desktop/img/June22/contact-lens-more.jpg"
+            alt="left"
+          />
+          <img
+            src=" https://static1.lenskart.com/media/desktop/img/June22/contact-lens-more-1.jpg"
+            alt="right"
+          />
+        </SimpleGrid>
+        <Box
+          textAlign={{ base: "center", md: "center" }}
+          m={2}
+          fontFamily={"sans-serif"}
+        >
+          <Text as="h3" size="lg" fontSize={[15, 15, 20, 25]}>
+            {windowWidth <= 768
+              ? "--- BUY IT YOUR WAY---"
+              : "___________________________BUY IT YOUR WAY _________________________"}
+          </Text>
+        </Box>
+        <SimpleGrid
+          gridTemplateColumns={"repeat(2,1fr)"}
+          gap={5}
+          w={"80%"}
+          m={"auto"}
+        >
+          <img
+            src=" https://static1.lenskart.com/media/desktop/img/republic/celb-new/call.png"
+            alt="Call"
+          />
+          <img
+            src=" https://static1.lenskart.com/media/desktop/img/republic/celb-new/wts-up.png"
+            alt="Whatapp"
+          />
+          <img
+            src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/hto.jpg"
+            alt="hto"
+          />
+          <img
+            src=" https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/stores.jpg"
+            alt="stores"
+          />
+        </SimpleGrid>
       </Box>
     </>
   );
