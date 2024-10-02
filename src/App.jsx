55bdfbdea7255ed08489851e8637759db2343c92
ashokdhinda <ashokkudhinda143@gmail.com>
2024-10-02 18:35:10 +0530

    contact page completd

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home"
 import SignIn from "./Pages/login/SignIn"
 import SignUp from "./Pages/login/SignUp"
 import Wishlist from "./Pages/Wishlist/Wishlist"
+import Contact from "./Pages/Contact/Contact"
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/wishlist" element={<Wishlist/> } />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/contact" element={<Contact/> } /> 
 </Routes>
     </>
   )
